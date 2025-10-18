b344c7174b914c5ae4aadc37a272b592d2420e7c
Ana <162636836+actiasAnii@users.noreply.github.com>
2025-10-18 09:55:42 -0700

    more setup + imported tmp

---
+++ Assets/Scripts/BugDataLoader.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class BugDataLoader : MonoBehaviour
+{
+    // singleton for loading data from the json
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
