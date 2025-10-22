e5bfdde1fcb32143ac9eeecde343f34a12295ded
vekim00100 <vekim@ucsc.edu>
2025-10-22 15:28:54 -0700

    Implementing a Start Scene
    - Start Scene with a Moving Background
    - However, Title, Start Button, Exit Button, and Particles are hidden
    * Will fix, but not complete to merge

---
+++ Assets/Scripts/StartMenuController.cs
@@ -0,0 +1,18 @@
+// using UnityEngine;
+// using UnityEngine.SceneManagement;
+
+// public class StartMenuController : MonoBehaviour
+// {
+//     public void onStartClick()
+//     {
+//         SceneManager.LoadScene("SampleScene");
+//     }
+
+//     public void onExitClick()
+//     {
+// # if UNITY_EDITOR
+//         UnityEditor.EditorApplication.isPlaing = false;
+// # endif
+//         EditorApplication.Quit();
+//     }
+// }
